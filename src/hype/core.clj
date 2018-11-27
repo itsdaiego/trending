6dd6b506566b9103d8544b4652100f57aec27b27
Dyego Lima <dyego9444@gmail.com>
2018-11-27 17:26:39 -0200

    parse fetched data

--- src/hype/core.clj
+++ src/hype/core.clj
@@ -2,20 +2,26 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [org.httpkit.client :as http]
-            [cheshire.core :refer :all]))
+            [cheshire.core :as json]))
 
-(defn fetchRepos
+(defn fetch-repos
   []
   @(http/get "https://api.github.com/search/repositories?q=stars:%3E1&per_page=2"))
 
-(defn parse
+(defn get-items
   [content]
-  (let [body (parse-string (get content :body))]
-    (println (type body))
-    (println (get body :total_count))))
+  (let [body (json/decode (get content :body))]
+    (get body "items")))
+
+(defn parse-data
+  [items]
+  (map 
+    (fn [item] (hash-map :name (get item "name"))) items))
 
 (defn -main
   [& args]
-  (-> (fetchRepos)
-      (parse)))
+  (-> (fetch-repos)
+      (get-items)
+      (parse-data)
+      (println)))
 
